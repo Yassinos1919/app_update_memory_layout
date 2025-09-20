ac7d5e42a14e8c3548d6ee49b79d81e95292fd8b
Yassinos1919 <yassinbenfaiza0@gmail.com>
2025-09-20 19:33:51 +0100

    project_added

---
+++ app_update/Core/Inc/esp82_66.h
@@ -0,0 +1,13 @@
+/*
+ * esp82_66.h
+ *
+ *  Created on: Sep 20, 2025
+ *      Author: yassi
+ */
+
+#ifndef INC_ESP82_66_H_
+#define INC_ESP82_66_H_
+
+
+
+#endif /* INC_ESP82_66_H_ */
---
+++ app_update/Core/Src/esp82_66.c
@@ -0,0 +1,8 @@
+/*
+ * esp82_66.c
+ *
+ *  Created on: Sep 20, 2025
+ *      Author: yassi
+ */
+
+
